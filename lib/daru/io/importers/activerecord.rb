a3f83b5c3ac4b465d8811af0a2b10ff19a95dcdf
Athitya <athityakumar@gmail.com>
2017-06-01 15:47:00 +0530

    Ports from_activerecord module - part 2
    Changes tests to subject & its format

--- lib/daru/io/importers/activerecord.rb
+++ lib/daru/io/importers/activerecord.rb
@@ -33,5 +33,3 @@ module Daru
     end
   end
 end
-
-
--- spec/daru/io/importers/activerecord_spec.rb
+++ spec/daru/io/importers/activerecord_spec.rb
@@ -8,83 +8,40 @@ module Daru::IO::Rspec
   end
 end
 
-# shared_context 'with accounts table in sqlite3 database' do
-#   let(:db_name) do
-#     'daru_test'
-#   end
-
-#   before do
-#     # just in case
-#     FileUtils.rm(db_name) if File.file?(db_name)
-
-#     SQLite3::Database.new(db_name).tap do |db|
-#       db.execute "create table accounts(id integer, name varchar, age integer, primary key(id))"
-#       db.execute "insert into accounts values(1, 'Homer', 20)"
-#       db.execute "insert into accounts values(2, 'Marge', 30)"
-#     end
-#   end
-
-#   after do
-#     FileUtils.rm(db_name)
-#   end
-# end
-
 RSpec.describe Daru::IO::Importers::Activerecord do
-      let(:db_name) { 'daru_test' }
+  let(:db_name) { 'daru_test' }
+  let(:relation) { Daru::IO::Rspec::Account.all }
 
-      before do
-        # just in case
-        FileUtils.rm(db_name) if File.file?(db_name)
-
-        SQLite3::Database.new(db_name).tap do |db|
-          db.execute "create table accounts(id integer, name varchar, age integer, primary key(id))"
-          db.execute "insert into accounts values(1, 'Homer', 20)"
-          db.execute "insert into accounts values(2, 'Marge', 30)"
-        end
-      end
-
-      after do
-        FileUtils.rm(db_name)
-      end
-
-      context 'with ActiveRecord::Relation' do
-        before do
-          Daru::IO::Rspec::Account.establish_connection "sqlite3:#{db_name}"
-        end
-
-        let(:relation) do
-          Daru::IO::Rspec::Account.all
-        end
-
-        context 'without specifying field names' do
-          subject do
-            Daru::DataFrame.from_activerecord(relation)
-          end
-
-          it 'loads data from an AR::Relation object' do
-            accounts = subject
-            expect(accounts.class).to eq Daru::DataFrame
-            expect(accounts.nrows).to eq 2
-            expect(accounts.vectors.to_a).to eq [:id, :name, :age]
-            expect(accounts.row[0][:id]).to eq 1
-            expect(accounts.row[0][:name]).to eq 'Homer'
-            expect(accounts.row[0][:age]).to eq 20
-          end
-        end
-
-        context 'with specifying field names in parameters' do
-          subject do
-            Daru::DataFrame.from_activerecord(relation, :name, :age)
-          end
-
-          it 'loads data from an AR::Relation object' do
-            accounts = subject
-            expect(accounts.class).to eq Daru::DataFrame
-            expect(accounts.nrows).to eq 2
-            expect(accounts.vectors.to_a).to eq [:name, :age]
-            expect(accounts.row[0][:name]).to eq 'Homer'
-            expect(accounts.row[0][:age]).to eq 20
-          end
-        end
-      end
+  before do
+    FileUtils.rm(db_name) if File.file?(db_name)
+    SQLite3::Database.new(db_name).tap do |db|
+      db.execute "create table accounts(id integer, name varchar, age integer, primary key(id))"
+      db.execute "insert into accounts values(1, 'Homer', 20)"
+      db.execute "insert into accounts values(2, 'Marge', 30)"
     end
+    Daru::IO::Rspec::Account.establish_connection "sqlite3:#{db_name}"
+  end
+
+  after do
+    FileUtils.rm(db_name)
+  end
+
+  context 'without specifying field names' do
+    subject { Daru::DataFrame.from_activerecord(relation) }
+
+    it { is_expected.to be_an(Daru::DataFrame) }
+    its(:nrows) { is_expected.to eq(2) }
+    its('vectors.to_a') { is_expected.to eq([:id, :name, :age]) }
+    its('id.to_a.first') { is_expected.to eq(1) }
+    it { is_expected.to eq(Daru::DataFrame.new([[1,2],['Homer', 'Marge'],[20, 30]],order: [:id, :name, :age])) }
+  end
+
+  context 'with specifying field names in parameters' do
+    subject { Daru::DataFrame.from_activerecord(relation, :name, :age) }
+
+    it { is_expected.to be_an(Daru::DataFrame) }
+    its(:nrows) { is_expected.to eq(2) }
+    its('vectors.to_a') { is_expected.to eq([:name, :age]) }
+    it { is_expected.to eq(Daru::DataFrame.new([['Homer', 'Marge'],[20, 30]],order: [:name, :age])) }
+  end
+end
