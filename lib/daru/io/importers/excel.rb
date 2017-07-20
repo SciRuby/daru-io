45a9fd7abbd21b020bb7b10f6c74cee91a48c90d
Athitya <athityakumar@gmail.com>
2017-07-20 17:36:25 +0530

    Reverts renaming worksheet_id -> sheet in Excel Importer

--- lib/daru/io/importers/excel.rb
+++ lib/daru/io/importers/excel.rb
@@ -15,22 +15,21 @@ module Daru
 
         # Imports a +Daru::DataFrame+ from an Excel file (.xls, or .xlsx formats)
         #
-        # @param path [String] Path of Excel file, where the
-        #   DataFrame is to be imported from.
-        # @param sheet [Integer] The index of the worksheet in the excel file,
+        # @param path [String] Path of Excel file, where the DataFrame is to be imported from.
+        # @param worksheet_id [Integer] The index of the worksheet in the excel file,
         #   from where the +Daru::DataFrame+ will be imported. By default, the first
-        #   worksheet has +sheet+ as 0. In general, the n-th worksheet has
-        #   its sheet as n-1.
+        #   worksheet has +:worksheet_id+ as 0. In general, the n-th worksheet has
+        #   its worksheet_id as n-1.
         #
-        #   If sheet option is not given, it is taken as 0 by default and the
+        #   If worksheet_id option is not given, it is taken as 0 by default and the
         #   +Daru::DataFrame+ will be imported from the first worksheet in the excel file.
         # @param headers [Boolean] Defaults to true. When set to true, first row of the
-        #   given sheet is used as the order of the Daru::DataFrame and data of
+        #   given worksheet_id is used as the order of the Daru::DataFrame and data of
         #   the Dataframe consists of the remaining rows.
         #
         # @return A +Daru::DataFrame+ imported from the given excel worksheet
         #
-        # @example Reading from a default worksheet of an Excel file
+        # @example Reading from a default workworksheet_id of an Excel file
         #   df = Daru::IO::Importers::Excel.new("test_xls.xls").call
         #   df
         #
@@ -44,7 +43,7 @@ module Daru
         #   #    5        6  Fernand      nil      nil      nil
         #
         # @example Reading from a specific worksheet of an Excel file
-        #   df = Daru::IO::Importers::Excel.new("test_xls.xls", sheet: 0).call
+        #   df = Daru::IO::Importers::Excel.new("test_xls.xls", worksheet_id: 0).call
         #   df
         #
         #   #=> #<Daru::DataFrame(6x5)>
@@ -55,16 +54,16 @@ module Daru
         #   #    3        4    Franz      nil    Paris      nil
         #   #    4        5   George      5.5     Tome    a,b,c
         #   #    5        6  Fernand      nil      nil      nil
-        def initialize(path, sheet: 0, headers: true)
+        def initialize(path, worksheet_id: 0, headers: true)
           optional_gem 'spreadsheet', '~> 1.1.1'
 
-          @path    = path
-          @sheet   = sheet
-          @headers = headers
+          @path         = path
+          @headers      = headers
+          @worksheet_id = worksheet_id
         end
 
         def call
-          worksheet = Spreadsheet.open(@path).worksheet(@sheet)
+          worksheet = Spreadsheet.open(@path).worksheet(@worksheet_id)
           headers   = if @headers
                         ArrayHelper.recode_repeated(worksheet.row(0)).map(&:to_sym)
                       else
