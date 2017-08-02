7f25ad36c1f62ff16c3fdbfa559aad2597cac463
Athitya <athityakumar@gmail.com>
2017-08-03 00:57:09 +0530

    Adds tests for remaining fixture files of RDS & RData

--- lib/daru/io/importers/r_data.rb
+++ lib/daru/io/importers/r_data.rb
@@ -10,7 +10,9 @@ module Daru
         #
         # @param path [String] Path to the RData file
         # @param variable [String] The variable to be imported from the
-        #   variables stored in the RData file
+        #   variables stored in the RData file. Please note that the R
+        #   variable to be imported from the RData file should be a
+        #   +data.frame+
         #
         # @return A +Daru::DataFrame+ imported from the given RData file and variable name
         #
@@ -28,13 +30,27 @@ module Daru
         #   #       ...        ...        ...        ...        ...        ...        ...  ...
         def initialize(path, variable)
           super(path)
-          @variable = variable.to_sym
+          @variable = variable.to_s
         end
 
         def call
           @instance = RSRuby.instance
           @instance.eval_R("load('#{@path}')")
+
+          validate_params
+
           process_dataframe(@instance.send(@variable.to_sym))
+        end
+
+        private
+
+        def validate_params
+          valid_r_dataframe_variables = @instance.eval_R('Filter(function(x) is.data.frame(get(x)) , ls())')
+          return if valid_r_dataframe_variables.include?(@variable)
+
+          variable_type = @instance.eval_R("typeof(#{@variable})")
+          raise ArgumentError, "Expected the given R variable (#{@variable}) to be a data.frame, got a "\
+                               "#{variable_type} instead."
         end
       end
     end
