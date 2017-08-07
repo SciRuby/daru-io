1271a9bfd47dee3f073eaf6b51bff4ea9aedf9ac
Athitya <athityakumar@gmail.com>
2017-08-07 23:32:40 +0530

    Adds spec test for Avro::Schema input of :schema in Avro Exporter

--- spec/daru/io/exporters/avro_spec.rb
+++ spec/daru/io/exporters/avro_spec.rb
@@ -15,28 +15,58 @@ RSpec.describe Daru::IO::Exporters::Avro do
   before { described_class.new(df, tempfile.path, schema).call }
 
   context 'writes DataFrame to an Avro file' do
-    let(:schema) do
-      {
-        'type' => 'record',
-        'name' => 'test',
-        'fields' => [
-          {'name' => 'a', 'type' => 'int'},
-          {'name' => 'b', 'type' => 'int'},
-          {'name' => 'c', 'type' => %w[int string]},
-          {'name' => 'd', 'type' => %w[int string null]}
+    context 'when schema is Hash' do
+      let(:schema) do
+        {
+          'type' => 'record',
+          'name' => 'test',
+          'fields' => [
+            {'name' => 'a', 'type' => 'int'},
+            {'name' => 'b', 'type' => 'int'},
+            {'name' => 'c', 'type' => %w[int string]},
+            {'name' => 'd', 'type' => %w[int string null]}
+          ]
+        }
+      end
+
+      it_behaves_like 'exact daru dataframe',
+        ncols: 4,
+        nrows: 5,
+        order: %w[a b c d],
+        data: [
+          [1,2,3,4,5],
+          [11,22,33,44,55],
+          ['a', 'g', 4, 5,'addadf'],
+          [nil, 23, 4,'a','ff']
         ]
-      }
     end
 
-    it_behaves_like 'exact daru dataframe',
-      ncols: 4,
-      nrows: 5,
-      order: %w[a b c d],
-      data: [
-        [1,2,3,4,5],
-        [11,22,33,44,55],
-        ['a', 'g', 4, 5,'addadf'],
-        [nil, 23, 4,'a','ff']
-      ]
+    context 'when schema is Avro::Schema' do
+      let(:schema) do
+        ::Avro::Schema.parse(
+          {
+            'type' => 'record',
+            'name' => 'test',
+            'fields' => [
+              {'name' => 'a', 'type' => 'int'},
+              {'name' => 'b', 'type' => 'int'},
+              {'name' => 'c', 'type' => %w[int string]},
+              {'name' => 'd', 'type' => %w[int string null]}
+            ]
+          }.to_json
+        )
+      end
+
+      it_behaves_like 'exact daru dataframe',
+        ncols: 4,
+        nrows: 5,
+        order: %w[a b c d],
+        data: [
+          [1,2,3,4,5],
+          [11,22,33,44,55],
+          ['a', 'g', 4, 5,'addadf'],
+          [nil, 23, 4,'a','ff']
+        ]
+    end
   end
 end
