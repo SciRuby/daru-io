a328841f55b34f69c77fd4a638da003b6d777b6f
Athitya <athityakumar@gmail.com>
2017-08-10 05:10:38 +0530

    Documents Exporters::Base#initialize method

--- lib/daru/io/exporters/base.rb
+++ lib/daru/io/exporters/base.rb
@@ -4,11 +4,29 @@ module Daru
   module IO
     module Exporters
       class Base < Daru::IO::Base
+        # Checks whether the first argument given to any +Daru::IO::Exporter+ module
+        # is a +Daru::DataFrame+. Raises an error when it's not a +Daru::DataFrame+.
+        #
+        # @param dataframe [Daru::DataFrame] A DataFrame to initialize
+        #
+        # @example Stores the dataframe
+        #   df = Daru::DataFrame.new([[1,2],[3,4]], order: [:a, :b])
+        #   Daru::IO::Exporters::Base.new(df)
+        #
+        #   #=> #<Daru::IO::Exporters::Base:0x007f899081af08 @dataframe=#<Daru::DataFrame(2x2)>
+        #   #      a   b
+        #   #  0   1   3
+        #   #  1   2   4>
+        #
+        # @example Raises error when not a DataFrame
+        #   Daru::IO::Exporters::Base.new(nil)
+        #
+        #   #=> ArgumentError: Expected first argument to be a Daru::DataFrame, received NilClass instead
         def initialize(dataframe)
           unless dataframe.is_a?(Daru::DataFrame)
             raise ArgumentError,
               'Expected first argument to be a Daru::DataFrame, '\
-              "received #{dataframe.class} instead."\
+              "received #{dataframe.class} instead."
           end
           @dataframe = dataframe
         end
