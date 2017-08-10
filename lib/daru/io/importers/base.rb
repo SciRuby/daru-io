4bf7dbcbda901c6f4348c1194c9fb4bae2ce5454
Athitya <athityakumar@gmail.com>
2017-08-10 05:41:22 +0530

    Documents Importers::Base#guess_parse method

--- lib/daru/io/importers/base.rb
+++ lib/daru/io/importers/base.rb
@@ -4,34 +4,42 @@ module Daru
   module IO
     module Importers
       class Base < Daru::IO::Base
+        # Guesses the +Daru::DataFrame+ from the parsed set of key-value pairs.
+        #
+        # @param keys [Array] A set of keys from given key-value pairs
+        # @param vals [Array] A set of values from given key-value pairs
+        #
+        # @example When key-value pairs contains values that is Array of Hashes
+        #   Daru::IO::Importers::Base.guess_parse([:a], [[{ x: 1, y: 2 },{ x: 3, y: 4 }]])
+        #
+        #   #=> #<Daru::DataFrame(2x2)>
+        #   #      x   y
+        #   #  0   1   2
+        #   #  1   3   4
+        #
+        # @example When key-value pairs contains values that is Arrays
+        #   Daru::IO::Importers::Base.guess_parse([:x, :y], [[1,3], [2,4]])
+        #
+        #   #=> #<Daru::DataFrame(2x2)>
+        #   #      x   y
+        #   #  0   1   2
+        #   #  1   3   4
+        #
+        # @example When key-value pairs contains Array of keys contain value Hashes
+        #   Daru::IO::Importers::Base.guess_parse([:a, :b], [{ x: 1, y: 2 }, { x: 3, y: 4 }])
+        #
+        #   #=> #<Daru::DataFrame(2x2)>
+        #   #      x   y
+        #   #  a   1   2
+        #   #  b   3   4
         def self.guess_parse(keys, vals)
           case vals.first
           when Array
             case vals.first.first
-            when Hash
-              # Array of hashes
-              # key a :
-              # [
-              #   { x: 1, y: 2 },
-              #   { x: 3, y: 4 }
-              # ]
-              Daru::DataFrame.new vals.flatten
-            else
-              # Hash containing Array
-              # key a :
-              # {
-              #   x: [1,2,3,4]
-              #   y: [5,6,7,8]
-              # }
-              Daru::DataFrame.rows vals.transpose, order: keys
+            when Hash then Daru::DataFrame.new(vals.flatten)
+            else Daru::DataFrame.rows(vals.transpose, order: keys)
             end
-          when Hash
-            # Array containing Hash
-            # [
-            #   key a: { x: 1, y: 2 }
-            #   key b: { x: 3, y: 4 }
-            # ]
-            Daru::DataFrame.new vals.flatten, index: keys
+          when Hash then Daru::DataFrame.new(vals.flatten, index: keys)
           end
         end
       end
