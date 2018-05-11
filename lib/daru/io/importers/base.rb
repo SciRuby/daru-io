16b02215d17b7773dd71258cecc4fd22c24a2925
Rohit Ner <rohitner1@gmail.com>
2018-05-12 01:46:23 +0530

    correct parse_hash method

--- lib/daru/io/importers/base.rb
+++ lib/daru/io/importers/base.rb
@@ -65,11 +65,15 @@ module Daru
               @max_line_length = max_line_length
               @line_divider    = line_divider
               @current_lineno  = 0
+              @file_format     = file_format
+              @current_source = File.expand_path(file)
               raw_list = []
               while line = logfile.gets(@line_divider, @max_line_length)
                 @current_lineno += 1
                 if request_data = @file_format.parse_line(line) { |wt, message| warn(wt, message) }
+                  request_data = request_data.merge(source: @current_source, lineno: @current_lineno)
                   @parsed_lines += 1
+                  update_current_request(request_data)
                   raw_hash = @file_format.request(request_data).attributes
                   raw_list << raw_hash if not raw_hash.nil?
                 end
@@ -78,10 +82,11 @@ module Daru
               raw_list.each do |hash|
                 parsed_list << hash if hash.key? :method
               end
-              for i in (0...parsed_list.size - 1)
-                j = raw_list.index(parsed_list[i+1])
-                k = raw_list.index(parsed_list[i]) + 1
-                for l in (k...j)
+              for i in (0...parsed_list.size)
+                j = raw_list.index(parsed_list[i])
+                k = raw_list.index(parsed_list[i+1])
+                k = k.nil? ? raw_list.size : k
+                for l in (j...k)
                   parsed_list[i].merge!(raw_list[l])
                 end
               end
